a4a1dd71cc0fcd99f99432622b62aeda781aa226
mayank-genesis <mayank16meena@gmail.com>
2020-06-07 10:55:46 +0530

    Add References to Main.java

--- CodeChef/JuneChallange2020/Main.java
+++ CodeChef/JuneChallange2020/Main.java
@@ -8,3 +8,9 @@ public class Main {
     public static void main(String[] args){
     }
 }
+/*
+References:
+BufferedReader - https://www.geeksforgeeks.org/java-io-bufferedreader-class-java/
+Scanner vs BufferedReader - https://www.geeksforgeeks.org/difference-between-scanner-and-bufferreader-class-in-java/
+Java8 parallel Sort - https://www.geeksforgeeks.org/java-8-arrays-parallelsort-method-with-examples/
+*/
