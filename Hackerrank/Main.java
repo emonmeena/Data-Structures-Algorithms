174009cda6f787cd28caa9f9632bdf60ba5d7d8d
mayank-genesis <mayank16meena@gmail.com>
2020-06-22 21:54:42 +0530

    comments added

--- Hackerrank/Main.java
+++ Hackerrank/Main.java
@@ -6,13 +6,13 @@ import Hackerrank.LinkedList.*;
 public class Main {
     public static void main(String[] args) {
 
-        // LinkedList
+        //Initializing LinkedList with head
         Node head = new Node(0);
-
+        // Inserting / Making LL
         for(int i = 0; i<11; i++){
         LinkedList.insertNodeAtLast(head, i*10);
         }
-
+        // Printing all elements
         LinkedList.printAll(head);
     }
 }
