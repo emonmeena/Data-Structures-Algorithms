5bd1edc88d167e8afd1176eea424e87f0e56bdb2
mayank-genesis <mayank16meena@gmail.com>
2020-06-23 17:46:57 +0530

    Adds Basic Template in Java

---
+++ BasicTemplate.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class BasicTemplate {
+    public static void main(String[] args) throws java.lang.Exception 
+        {
+            Scanner sc = new Scanner(System.in);
+    
+            int t = sc.nextInt(), n, k;
+            while(t-- > 0){
+                n = sc.nextInt();
+                k = sc.nextInt();
+    
+                Solve(n, k);
+            }
+        }
+    
+        private static void Solve(int n, int k) {
+            
+        }
+
+}
--- Hackerrank/Main.java
+++ Hackerrank/Main.java
@@ -1,31 +1,9 @@
 package Hackerrank;
 
-import Hackerrank.LinkedList.*;
-
-
 public class Main {
     public static void main(String[] args) {
 
-        //Initializing LinkedList with head
-        Node head = new Node(0);
-        // Inserting / Making LL
-        for(int i = 0; i<11; i++){
-        LinkedList.insertNodeAtLast(head, i*10);
-        }
-        // Printing all elements
-        LinkedList.printAll(head);
-
-        int key = 106;
-        // Search Method returns true or false
-        System.out.println( 
-        LinkedList.search(head, key)
-        );
-        // Insertion at the beginning
-        head = LinkedList.insertAtStart(head, key); //returns new head at new Node. coz head node changes
-
-        //inserting at Specific pos.
-        int pos = 3; // 1 < pos < last position
-        LinkedList.insert(head, pos, key);
-
+        
     }
+
 }
