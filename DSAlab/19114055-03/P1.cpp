f615990f1bf2731867a8cd2327beaa2fb1acee3d
mayank-genesis <mayank16meena@gmail.com>
2020-09-30 18:55:51 +0530

    L3

--- DSAlab/19114055-03/P1.cpp
+++ DSAlab/19114055-03/P1.cpp
@@ -3,7 +3,7 @@ Masthead -
 Submitted by - Mayank Meena 
 Enroll no. - 19114055
 Batch - O-3
-Programming language - C++17
+Programming language - C++17.
 Code Editor - VSCode 2020
 Terminal - WSL Ubuntu 2020
 Shell - git bash
@@ -20,71 +20,161 @@ Twitter - https://twitter.com/Meina_Mk
 #include<bits/stdc++.h>
 using namespace std;
 
-const int rows=5, cols=6;
+// Declairing the basic variables without using string, hence optimizing the space.
+    char passID[3], opt[2], colTemp;
+    int x, y, ele;
 
-void showSeats(int seats[][cols])
+const int rows=5, cols=6;
+int seats[rows][cols] = {0};
+
+bool isAvailable(int x, int y)
 {
+    if(seats[x][y] == 0) return true;
+    return false;
+}
+
+void push()
+{
+    int e = (y < 5)?4:9;
+    for(int i=e; i>y; i--)
+    {
+        if(!isAvailable(x, i))
+        {
+            cout<<"Pop P"<<seats[x][i]<<"\n";
+        }
+    }
+    seats[x][y] = ele;
+    for(int i=y; i<=e; i++)
+    {
+        if(!isAvailable(x, i))
+        {
+            cout<<"Push P"<<seats[x][i]<<"\n";
+        }
+    }
+}
+
+void exit()
+{
+    if(seats[x][y] == 0)
+    cout<<"Error: Passanger does not exist at requested position.\n";
+    else
+    {
+        int e = (y < 5)?4:9;
+        for(int i=e; i>=y; i--)
+        {
+            if(!isAvailable(x, i))
+            {
+                cout<<"Pop P"<<seats[x][i]<<"\n";
+            }
+        }
+        seats[x][y] = 0;
+        for(int i=y+1; i<=e; i++)
+        {
+            if(!isAvailable(x, i))
+            {
+                cout<<"Push P"<<seats[x][i]<<"\n";
+            }
+        }
+
+    }
+}
+
+void printSeats()
+{
+    cout<<"Flight occupancy status is: \n";
     for(int i=0; i<rows; i++)
     {
         for(int j=0; j<cols; j++)
         {
-            if(j == cols/2+1) cout<<" || ";
+            if(j == cols/2)
+            cout<<"|| ";
+            if(seats[i][j] != 0)
+            cout<<"P";
             cout<<seats[i][j]<<" ";
+
         }
         cout<<"\n";
     }
 }
 
-void solve(){
-    int seats[rows][cols] = {0};
-
-    // Input 
-    char ch[6], opt;
-    
-    cin.getline(ch, 7);
-    // seats[ch[3]-49][ch[5]-65] = ch[1]-48;
-    
-     while(true)
+void fullExit()
+{
+    cout<<"The Lifo order of passanger exit is:\n";
+    for(int i=0; i<rows; i++)
     {
-        // cout<<"Enter your choice: \n"<<"I for Insert\n"<<"D for Delete\n"<<"E for Exit\n"<<"P for Print\n";
-        cin>>opt;
-        if(opt == 'E1')
-        {
-            
-            break;
-        }    
-        if(opt == 'E2')
-        {
-            cout<<"Exit: ";
+        for(int j=2; j>-1; j--)
+        {   
+            if(!isAvailable(i, j))
+            cout<<"Pop P"<<seats[i][j]<<"\n";
         }
-        if(opt == 'E')
+        for(int j=5; j>2; j--)
         {
-            cout<<"The program has sopped. ";
-        }
-        if(opt == 'P')
-        {
-            cout<<"The deleted element is: ";
-        }
-        if(opt == 'X')
-        {
-            cout<<"Flight occupancy  status is:\n ";
-            showSeats(seats);
+            if(!isAvailable(i, j))
+            cout<<"Pop P"<<seats[i][j]<<"\n";
         }
     }
 }
 
+void processInput()
+{
+    // Taking the inputs
+            cin>>passID;
+            cin>>x;
+            cin>>colTemp;
+            // Few conversion step to optimize the space avoiding string.
+            char* ref = strtok(passID, "P");
+            y = colTemp-65;
+            ele = stoi(ref);
+            x--;
+}
+
+void solve()
+{
+    
+
+    while(true)
+    {
+        // cout<<"Enter your choice\n"<<"​E1​ for Passenger Entry\n"<<"​E2​ for Passenger Exit\n";
+        // cout<<"E​ for Program Exit\n"<<"P​ for Print\n"<<"X​ for Full Exit: ​​E2\n";
+        cin>>opt;
+
+        if(opt[1] == '1')
+        {
+        // cout<<"entry\n";
+            processInput();
+
+            if(isAvailable(x, y))
+            push();
+            else cout<<"Error: Seat already occupied.\n";
+        }
+        else if(opt[1] == '2')
+        {
+        cout<<"exit\n";
+            processInput();
+            exit();
+        }
+
+        else if(opt[0] == 'E')
+        // cout<<"break\n";
+            break;
+
+        if(opt[0] == 'P')
+        // cout<<"Print\n";
+            printSeats();
+
+        if(opt[0] == 'X')
+        // cout<<"fullExit\n";
+            fullExit();
+    }
+
+    cout<<"P"<<ele<<" "<<x<<" "<<y<<"\n";
+}
+
 int main(int argc, char const *argv[])
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);    
+// ios_base::sync_with_stdio(false);
+// cin.tie(NULL);
 
     solve();
     return 0;
 }
-
-/*
-References - 
-https://www.geeksforgeeks.org/extract-integers-string-c/
-https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
-https://www.geeksforgeeks.org/pass-2d-array-parameter-c/
-*/
